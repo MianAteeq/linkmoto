413a0afaa23b824cded713b331e6a7161886dbba
MianAteeq <31040996+MianAteeq@users.noreply.github.com>
2026-01-19 15:16:38 +0500

    Refactor LoginController for improved login handling

--- Modules/Admin/Http/Controllers/LoginController.php
+++ Modules/Admin/Http/Controllers/LoginController.php
@@ -3,49 +3,59 @@
 namespace Modules\Admin\Http\Controllers;
 
 use Illuminate\Contracts\Support\Renderable;
+use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use App\Models\Admin;
-use Auth;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
 
 class LoginController extends Controller
 {
+     use ThrottlesLogins;
+
+    public function username()
+    {
+        return 'email';
+    }
     public function index()
     {
         return view('admin::admin.auth.login');
     }
 
 
+  
+
     public function store(Request $request)
-{
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-    ]);
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
 
-    if ($this->hasTooManyLoginAttempts($request)) {
-        $this->fireLockoutEvent($request);
-        return $this->sendLockoutResponse($request);
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+            return $this->sendLockoutResponse($request);
+        }
+
+        $user = Admin::where('email', $request->email)->first();
+
+        if ($user && $user->status != 1) {
+            return redirect('/admin/login')->withErrors('User is Not Active!');
+        }
+
+        if (Auth::guard('admin')->attempt(
+            ['email' => $request->email, 'password' => $request->password],
+            $request->boolean('remember')
+        )) {
+            $this->clearLoginAttempts($request);
+            return redirect('admin/dashboard');
+        }
+
+        $this->incrementLoginAttempts($request);
+
+        return redirect('/admin/login')->withErrors('Please Enter Valid Email ID or Password.');
     }
-
-    $user = Admin::where('email', $request->email)->first();
-
-    if ($user && $user->status != 1) {
-        return redirect('/admin/login')->withErrors('User is Not Active!');
-    }
-
-    if (Auth::guard('admin')->attempt(
-        ['email' => $request->email, 'password' => $request->password],
-        $request->boolean('remember')
-    )) {
-        $this->clearLoginAttempts($request);
-        return redirect()->route('admin.dashboard'); // or admin/dashboard
-    }
-
-    $this->incrementLoginAttempts($request);
-
-    return redirect('/admin/login')->withErrors('Please Enter Valid Email ID or Password.');
-}
 
 
 
