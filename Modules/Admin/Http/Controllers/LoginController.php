4bcc17fb49ab2173d734bd54bac392801e52267a
MianAteeq <31040996+MianAteeq@users.noreply.github.com>
2026-01-19 15:19:48 +0500

    Update LoginController.php

--- Modules/Admin/Http/Controllers/LoginController.php
+++ Modules/Admin/Http/Controllers/LoginController.php
@@ -11,8 +11,7 @@ use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-     use ThrottlesLogins;
-
+   
     public function username()
     {
         return 'email';
