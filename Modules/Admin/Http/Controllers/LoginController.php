94f0f3acb3691fcdf1d78ed506f688ae2a03f8d7
MianAteeq <31040996+MianAteeq@users.noreply.github.com>
2026-01-19 15:19:15 +0500

    Update LoginController.php

--- Modules/Admin/Http/Controllers/LoginController.php
+++ Modules/Admin/Http/Controllers/LoginController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Foundation\Auth\ThrottlesLogins;
 
 class LoginController extends Controller
 {
