ec0c34125fd4f69daa504f4a6cdf2747be378d52
Mohammad Atique <ateeq.adrees86@gmail.com>
2026-03-31 13:58:48 +0500

    bug fix

--- Modules/Vender/Http/Controllers/InvoiceController.php
+++ Modules/Vender/Http/Controllers/InvoiceController.php
@@ -162,6 +162,7 @@ class InvoiceController extends Controller
             // 8️⃣ Send email
             Mail::send('email.invoice', $data, function ($message) use ($data, $files, $request) {
                 $message->to($request->email)
+                    ->from(config('mail.from.address'), 'Service Provider Business Name (via Motonos)') // ✅ static
                     ->subject('Invoice Reminder');
 
                 foreach ($files as $file) {
