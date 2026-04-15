49f9f287f9c86722f2b78f25886cdd8cccaa66f6
Mohammad Atique <ateeq.adrees86@gmail.com>
2026-04-15 17:43:56 +0500

    bug fix

--- Modules/Vender/Http/Controllers/Api/ContactController.php
+++ Modules/Vender/Http/Controllers/Api/ContactController.php
@@ -26,15 +26,15 @@ class ContactController extends Controller
                 'name' =>  ['required', 'string', 'max:255'],
                 'last_name' => 'required',
                 // 'mobile_no' => 'required|unique:contact_details,mobile_no',
-                // 'email' => 'required|unique:contact_details,email',
+                'email' => 'required|string|email:rfc,dns|filled',
                 // 'address' => 'required',
                 // 'city' => 'required',
                 // 'postal_code' => 'required',
 
 
             ]);
-            
-            
+
+
 
             if ($validator->fails()) {
 
@@ -46,21 +46,21 @@ class ContactController extends Controller
 
 
             $vender_id = $request->user()->vender_id == 0 ? $request->user()->id : $request->user()->vender_id;
-            $email_exist=ContactDetail::where('vender_id',$vender_id)->where('email',$request['email'])->first();
-            
-            if(!empty($email_exist)){
-                 $responseArr['message'] = 'Email Already Taken!';
+            $email_exist = ContactDetail::where('vender_id', $vender_id)->where('email', $request['email'])->first();
+
+            if (!empty($email_exist)) {
+                $responseArr['message'] = 'Email Already Taken!';
                 $responseArr['token'] = '';
                 $responseArr['status'] = false;
                 return response()->json($responseArr);
             }
-            
+
             $trading_id = User::find($request->user()->id)['default_trading_unit'];
 
 
-            $exist=ContactDetail::where('vender_id',$vender_id)->where('mobile_no',$request['mobile_no'])->count();
+            $exist = ContactDetail::where('vender_id', $vender_id)->where('mobile_no', $request['mobile_no'])->count();
 
-            if($exist>0) {
+            if ($exist > 0) {
 
                 $responseArr['message'] = 'Contact Already Exist';
                 $responseArr['token'] = '';
@@ -72,12 +72,12 @@ class ContactController extends Controller
 
             $contact_detail = ContactDetail::create([
                 "vender_id" => $vender_id,
-                "contact_no"=>'CON-'."SVP".str_pad($vender_id, 5, "0", STR_PAD_LEFT)."-". str_pad($latestOrder?$latestOrder->id+1: 0 + 1, 5, "0", STR_PAD_LEFT),
-                'trading_id'=>$trading_id,
+                "contact_no" => 'CON-' . "SVP" . str_pad($vender_id, 5, "0", STR_PAD_LEFT) . "-" . str_pad($latestOrder ? $latestOrder->id + 1 : 0 + 1, 5, "0", STR_PAD_LEFT),
+                'trading_id' => $trading_id,
                 ...$request->all(),
             ]);
 
-            $contacts = ContactDetail::with('vehicles','quotes','bookings', 'vehicles.vehicle_make')->withCount('quotes')->withCount('bookings')->withCount('jobs')->where('vender_id', $vender_id)->orderBy('id','desc')->first();
+            $contacts = ContactDetail::with('vehicles', 'quotes', 'bookings', 'vehicles.vehicle_make')->withCount('quotes')->withCount('bookings')->withCount('jobs')->where('vender_id', $vender_id)->orderBy('id', 'desc')->first();
 
             return response()->json([
                 'status' => true,
@@ -119,33 +119,30 @@ class ContactController extends Controller
             $vender_id = $request->user()->vender_id == 0 ? $request->user()->id : $request->user()->vender_id;
             $trading_id = User::find($request->user()->id)['default_trading_unit'];
             $contacts = ContactDetail::with([
-        'vehicles',
-        'quotes',
-        'bookings',
-        'vehicles.vehicle_make',
-        'vehicles.vehicle_model',
-        'jobs' => function ($q) use ($trading_id) {
-            $q->where('trading_id', $trading_id);   // 👈 added condition in jobs
-        },
-        'jobs.vehicle',
-        'jobs.contact_detail',
-        'jobs.job_requests',
-        'jobs.job_requests.job_types',
-        'jobs.job_requests.job_types.job_type'
-    ])->withCount([
-                'quotes as quotes_count' => function ($q) use ($trading_id) {
-            $q->where('trading_id', $trading_id);             
-                    
+                'vehicles',
+                'quotes',
+                'bookings',
+                'vehicles.vehicle_make',
+                'vehicles.vehicle_model',
+                'jobs' => function ($q) use ($trading_id) {
+                    $q->where('trading_id', $trading_id);   // 👈 added condition in jobs
                 },
-            'bookings as bookings_count' => function ($q) use ($trading_id) {
-            $q->where('trading_id', $trading_id);             
-                
-            },
-            'jobs as jobs_count' => function ($q) use ($trading_id) {
-            $q->where('trading_id', $trading_id);             
-                
-            }
-            ])->where('vender_id', $vender_id)->where('contact_no','!=',null)->orderBy('id','desc')->find($request['contact_id']);
+                'jobs.vehicle',
+                'jobs.contact_detail',
+                'jobs.job_requests',
+                'jobs.job_requests.job_types',
+                'jobs.job_requests.job_types.job_type'
+            ])->withCount([
+                'quotes as quotes_count' => function ($q) use ($trading_id) {
+                    $q->where('trading_id', $trading_id);
+                },
+                'bookings as bookings_count' => function ($q) use ($trading_id) {
+                    $q->where('trading_id', $trading_id);
+                },
+                'jobs as jobs_count' => function ($q) use ($trading_id) {
+                    $q->where('trading_id', $trading_id);
+                }
+            ])->where('vender_id', $vender_id)->where('contact_no', '!=', null)->orderBy('id', 'desc')->find($request['contact_id']);
             return response()->json([
                 'status' => true,
                 'contact_detail' => $contacts,
@@ -168,7 +165,7 @@ class ContactController extends Controller
             $vender_id = $request->user()->vender_id == 0 ? $request->user()->id : $request->user()->vender_id;
             $trading_id = User::find($request->user()->id)['default_trading_unit'];
 
-            $contacts = ContactDetail::with('vehicles','quotes','bookings', 'vehicles.vehicle_make' )->withCount('quotes')->withCount('bookings')->withCount('jobs')->where('vender_id', $vender_id)->where('contact_no','!=',null)->orderBy('id','desc')->paginate(10);
+            $contacts = ContactDetail::with('vehicles', 'quotes', 'bookings', 'vehicles.vehicle_make')->withCount('quotes')->withCount('bookings')->withCount('jobs')->where('vender_id', $vender_id)->where('contact_no', '!=', null)->orderBy('id', 'desc')->paginate(10);
             return response()->json([
                 'status' => true,
                 'contact_details' => $contacts,
@@ -232,6 +229,7 @@ class ContactController extends Controller
                 'id' =>  ['required'],
                 'name' =>  ['required', 'string', 'max:255'],
                 'last_name' => 'required',
+                'email' => 'required|string|email:rfc,dns|filled',
                 // 'mobile_no' => 'required|unique:contact_details,mobile_no,' . $request->id,
                 // 'email' => 'required|unique:contact_details,email,' . $request->id,
 
@@ -249,20 +247,20 @@ class ContactController extends Controller
 
 
             $vender_id = $request->user()->vender_id == 0 ? $request->user()->id : $request->user()->vender_id;
-            
-            $email_exist=ContactDetail::where('vender_id',$vender_id)->where('email',$request['email'])->where('id','!=',$request['id'])->first();
-            
-            if(!empty($email_exist)){
-                 $responseArr['message'] = 'Email Already Taken!';
+
+            $email_exist = ContactDetail::where('vender_id', $vender_id)->where('email', $request['email'])->where('id', '!=', $request['id'])->first();
+
+            if (!empty($email_exist)) {
+                $responseArr['message'] = 'Email Already Taken!';
                 $responseArr['token'] = '';
                 $responseArr['status'] = false;
                 return response()->json($responseArr);
             }
-            
 
-            $exist=ContactDetail::where('vender_id',$vender_id)->where('mobile_no',$request['mobile_no'])->where('id','!=',$request['id'])->count();
 
-            if($exist>0) {
+            $exist = ContactDetail::where('vender_id', $vender_id)->where('mobile_no', $request['mobile_no'])->where('id', '!=', $request['id'])->count();
+
+            if ($exist > 0) {
 
                 $responseArr['message'] = 'Contact Already Exist';
                 $responseArr['token'] = '';
@@ -270,17 +268,17 @@ class ContactController extends Controller
                 return response()->json($responseArr);
             }
 
-             ContactDetail::find($request['id'])->update([
+            ContactDetail::find($request['id'])->update([
                 "vender_id" => $vender_id,
                 ...$request->all(),
             ]);
 
-            $contacts = ContactDetail::with('vehicles','quotes','bookings', 'vehicles.vehicle_make')->withCount('quotes')->withCount('bookings')->withCount('jobs')->where('vender_id', $vender_id)->find($request['id']);
+            $contacts = ContactDetail::with('vehicles', 'quotes', 'bookings', 'vehicles.vehicle_make')->withCount('quotes')->withCount('bookings')->withCount('jobs')->where('vender_id', $vender_id)->find($request['id']);
             return response()->json([
-                    'status' => true,
-                    'contact_detail' => $contacts,
-                    'message' => "ContactDetail Update Successfully",
-                ]);
+                'status' => true,
+                'contact_detail' => $contacts,
+                'message' => "ContactDetail Update Successfully",
+            ]);
         } catch (Exception $e) {
 
             return response()->json([
@@ -313,55 +311,51 @@ class ContactController extends Controller
             }
 
 
-             $request['id'];
+            $request['id'];
 
-           $quote=Quotation::where('contact_detail_id',$request['id'])->count();
-            $booking=Booking::where('contact_detail_id',$request['id'])->count();
-            $vehicle=Vehicle::where('contact_id',$request['id'])->count();
+            $quote = Quotation::where('contact_detail_id', $request['id'])->count();
+            $booking = Booking::where('contact_detail_id', $request['id'])->count();
+            $vehicle = Vehicle::where('contact_id', $request['id'])->count();
 
-            if($quote>0){
+            if ($quote > 0) {
 
                 return response()->json([
                     'status' => false,
 
                     'message' => "ContactDetail Linked with Quotation First Delete Prerequisites First",
                 ]);
-
             }
-            if($booking>0){
+            if ($booking > 0) {
 
                 return response()->json([
                     'status' => false,
 
                     'message' => "ContactDetail Linked with Booking First Delete Prerequisites First",
                 ]);
-
             }
-            if($vehicle>0){
+            if ($vehicle > 0) {
 
                 return response()->json([
                     'status' => false,
 
                     'message' => "ContactDetail Linked with Vehicle First Delete Prerequisites First",
                 ]);
-
             }
 
 
-            $contact=ContactDetail::find($request['id']);
-            if(isset($contact)){
+            $contact = ContactDetail::find($request['id']);
+            if (isset($contact)) {
                 $contact->delete();
-
             }
 
 
 
 
             return response()->json([
-                    'status' => true,
+                'status' => true,
 
-                    'message' => "ContactDetail Delete Successfully",
-                ]);
+                'message' => "ContactDetail Delete Successfully",
+            ]);
         } catch (Exception $e) {
 
             return response()->json([
