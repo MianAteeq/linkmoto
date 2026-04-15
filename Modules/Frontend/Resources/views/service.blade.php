3c47c20009c3b9ed139ce9bbd748795bf01a4c69
ateeqadrees <ateeqadrees83@gmail.com>
2026-04-15 16:17:55 +0500

    Changings in blade files

--- Modules/Frontend/Resources/views/service.blade.php
+++ Modules/Frontend/Resources/views/service.blade.php
@@ -1,50 +1,62 @@
 @extends('frontend::layout.app')
-@section('content')
-    <!-- Page Title -->
-    <div class="page-title-area">
-        <img src="@if(!empty($setting['servicesBanner'])) {{$setting['servicesBanner']}} @else {{asset('modules/website/assets/img/home-one/footer-car.png')}} @endif" alt="Title">
-        <div class="container">
-            <div class="page-title-content">
-                <h2>Services</h2>
-                <ul>
-                    <li>
-                        <a href="<?php echo route('website.index'); ?>">Home</a>
-                    </li>
-                    <li>
-                        <i class='bx bx-chevron-right'></i>
-                    </li>
-                    <li>Services</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <!-- End Page Title -->
 
-    <!-- Service -->
-    <section class="pt-100 pb-70">
-        <div class="container">
-            <div class="row">
-              @foreach ($services as $service)
-                <div class="col-sm-6 col-lg-4">
-                    <a >
-                        <div class="service-item">
-                            <div class="service-img">
-                                <img src="{{ asset($service['image']??'modules/website/assets/img/home-one/service/1.jpg') }}"
-                                    alt="Service">
-                            </div>
-                            <div class="service-content">
-                                <i class='bx bx-car'></i>
-                                <i class='bx bx-car service-icon'></i>
-                                <h3>{{ $service['name'] }}</h3>
-                                <p>{{ $service['description'] }} </p>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                
-                @endforeach
+@section('css')
+<style>
+/* Responsive padding for mobile devices */
+@media (max-width: 768px) {
+  .pt-100 {
+    padding-top: 50px !important;
+  }
+
+  .pb-70 {
+    padding-bottom: 30px !important;
+  }
+}
+</style>
+@endsection
+
+@section('content')
+<div class="page-title-area">
+  <img
+    src="@if(!empty($setting['servicesBanner'])) {{$setting['servicesBanner']}} @else {{asset('modules/website/assets/img/home-one/footer-car.png')}} @endif"
+    alt="Title" class="img-fluid w-100">
+  <div class="container">
+    <div class="page-title-content">
+      <h2>Services</h2>
+      <ul>
+        <li>
+          <a href="<?php echo route('website.index'); ?>">Home</a>
+        </li>
+        <li>
+          <i class='bx bx-chevron-right'></i>
+        </li>
+        <li>Services</li>
+      </ul>
+    </div>
+  </div>
+</div>
+<section class="pt-100 pb-70">
+  <div class="container">
+    <div class="row">
+      @foreach ($services as $service)
+      <div class="col-12 col-sm-6 col-lg-4 mb-4">
+        <a href="javascript:void(0)" class="d-block h-100">
+          <div class="service-item h-100">
+            <div class="service-img">
+              <img src="{{ asset($service['image']??'modules/website/assets/img/home-one/service/1.jpg') }}"
+                alt="Service" class="img-fluid w-100">
             </div>
-        </div>
-    </section>
-    <!-- End Service -->
-    @endsection
+            <div class="service-content">
+              <i class='bx bx-car'></i>
+              <i class='bx bx-car service-icon'></i>
+              <h3>{{ $service['name'] }}</h3>
+              <p>{{ $service['description'] }} </p>
+            </div>
+          </div>
+        </a>
+      </div>
+      @endforeach
+    </div>
+  </div>
+</section>
+@endsection
--- Modules/Frontend/Resources/views/sign-in.blade.php
+++ Modules/Frontend/Resources/views/sign-in.blade.php
@@ -1,57 +1,70 @@
 @extends('frontend::layout.app')
-@section('content')
-<!-- Page Title -->
-<!--<div class="page-title-area">-->
-<!--    <img src="<?php echo asset('modules/website'); ?>/assets/img/home-one/footer-car.png" alt="Title">-->
-<!--    <div class="container">-->
-<!--        <div class="page-title-content">-->
-<!--            <h2>Sign In</h2>-->
-<!--            <ul>-->
-<!--                <li>-->
-<!--                    <a href="<?php echo route('website.index'); ?>">Home</a>-->
-<!--                </li>-->
-<!--                <li>-->
-<!--                    <i class='bx bx-chevron-right'></i>-->
-<!--                </li>-->
-<!--                <li>Sign In</li>-->
-<!--            </ul>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
-<!-- End Page Title -->
 
-<!-- Login -->
+@section('css')
+<style>
+/* Responsive padding adjustments */
+@media (max-width: 768px) {
+  .login-area.ptb-100 {
+    padding-top: 50px !important;
+    padding-bottom: 50px !important;
+  }
+
+  .login-item {
+    padding: 30px 20px !important;
+  }
+}
+
+/* Ensure the login item is properly contained */
+.login-item {
+  margin: 0 auto;
+  width: 100%;
+}
+</style>
+@endsection
+
+@section('content')
 <div class="login-area ptb-100">
   <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-8 col-lg-6">
 
-    <div class="login-item">
-      <h2>Sign in to Motonos</h2>
-      @foreach ($errors->all() as $error)
-      <div class="alert alert-danger alert-block">
+        <div class="login-item">
+          <h2 class="text-center">Sign in to Motonos</h2>
 
-        <strong>{{ $error }}</strong>
+          @foreach ($errors->all() as $error)
+          <div class="alert alert-danger alert-block">
+            <strong>{{ $error }}</strong>
+          </div>
+          @endforeach
+
+          <form method="POST" action="{{ route('website.vendor.login.submit') }}">
+            @csrf
+            <div class="form-group mb-3">
+              <label class="mb-1">Email:</label>
+              <input type="text" name="email" class="form-control" placeholder="Email">
+            </div>
+
+            <div class="form-group mb-1">
+              <label class="mb-1">Password:</label>
+              <input type="password" name="password" class="form-control" placeholder="Password">
+            </div>
+
+            <div class="text-end mb-3">
+              <a href="{{ route('forget.password') }}" class="small text-muted">Forgot password?</a>
+            </div>
+
+            <div class="text-center mb-3">
+              <button type="submit" class="btn login-btn w-100">Login</button>
+            </div>
+          </form>
+
+          <div class="text-center">
+            <span>Not yet registered? <a href="<?php echo route('website.vendor.register'); ?>">Click Here</a></span>
+          </div>
+        </div>
+
       </div>
-      @endforeach
-      <form method="POST" action="{{ route('website.vendor.login.submit') }}">
-        @csrf
-        <div class="form-group">
-          <label>Email:</label>
-          <input type="text" name="email" class="form-control" placeholder="Email">
-        </div>
-        <div class="form-group">
-          <label>Password:</label>
-          <input type="password" name="password" class="form-control" placeholder="Password">
-        </div>
-        <a href="{{ route('forget.password') }}" style="float: right;     margin-top: -26px;">Forgot
-          password?</a>
-        <div class="text-center">
-          <button type="submit" class="btn login-btn">Login</button>
-        </div>
-      </form>
-
-      <span>Not yet registered? <a href="<?php echo route('website.vendor.register'); ?>">Click Here</a></span>
     </div>
   </div>
 </div>
-<!-- End Login -->
 @endsection
