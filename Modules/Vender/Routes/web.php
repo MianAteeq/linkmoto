c8cdcdb6ade1292175b40189205c5605b4478dcb
ateeqadrees <ateeqadrees@fissionmonster.com>
2026-06-10 16:43:53 +0500

    bug fix

--- Modules/Vender/Routes/web.php
+++ Modules/Vender/Routes/web.php
@@ -38,7 +38,7 @@ use Modules\Vender\Http\Controllers\WorkStreamController;
 
 // routes under the writer
 
-Route::domain('business.motonos.com')->group(function () {
+Route::group(function () {
 
 Route::name('vender.')->middleware('auth:web')->group(function () {
 
